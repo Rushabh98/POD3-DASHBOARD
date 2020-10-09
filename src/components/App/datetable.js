3a43dd11e5fd54757fa699832a7ea6bcfecd89f9
Subham Gupta <subham.gupta@publicissapient.com>
2020-10-09 10:03:26 +0530

    Adding

---
+++ src/components/App/datetable.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: '#666699',
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}))(TableRow);
+
+function createData(Meeting_Date) {
+  return { Meeting_Date};
+}
+
+const rows = [
+  createData('12/12/2020'),
+  createData('12/12/2020'),
+  createData('12/12/2020'),
+  createData('12/12/2020'),
+  createData('12/12/2020'),
+  createData('12/12/2020'),
+  createData('12/12/2020'),
+  createData('12/12/2020'),
+];
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 100,
+    minHeight:300
+  },
+});
+
+export default function DateTables() {
+  const classes = useStyles();
+
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell align ='center'>Meeting_Date</StyledTableCell>
+            
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <StyledTableRow key={row.Meeting_Date}>
+              <StyledTableCell align='center' component="th" scope="row">
+               <a href=''>{row.Meeting_Date}</a> 
+              </StyledTableCell>
+              
+              
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
---
+++ src/components/App/table.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const StyledTableCell = withStyles((theme) => ({
+  head: {
+    backgroundColor: '#666699',
+    color: theme.palette.common.white,
+  },
+  body: {
+    fontSize: 14,
+  },
+}))(TableCell);
+
+const StyledTableRow = withStyles((theme) => ({
+  root: {
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+  },
+}))(TableRow);
+
+function createData(Serial_No,Meeting_Id,Subject_of_Mail,Customer_Emailid
+    ,Dealer_enduser_slot_start_date,Dealer_enduser_slot_start_time,Dealer_enduser_slot_end_time) {
+  return { Serial_No,Meeting_Id,Subject_of_Mail,Customer_Emailid
+    ,Dealer_enduser_slot_start_date,Dealer_enduser_slot_start_time,Dealer_enduser_slot_end_time};
+}
+
+const rows = [
+  createData(1,1,'Buying new car','abs@gmail.com','12/12/2020','11:10:02','12:23:23'),
+  createData(2,1,'Buying new car','abs@gmail.com','12/12/2020','11:10:02','12:23:23'),
+  createData(3,1,'Buying new car','abs@gmail.com','12/12/2020','11:10:02','12:23:23'),
+  createData(4,1,'Buying new car','abs@gmail.com','12/12/2020','11:10:02','12:23:23'),
+  createData(5,1,'Buying new car','abs@gmail.com','12/12/2020','11:10:02','12:23:23'),
+  createData(6,1,'Buying new car','abs@gmail.com','12/12/2020','11:10:02','12:23:23'),
+  createData(7,1,'Buying new car','abs@gmail.com','12/12/2020','11:10:02','12:23:23'),
+  createData(8,1,'Buying new car','abs@gmail.com','12/12/2020','11:10:02','12:23:23'),
+  createData(8,1,'Buying new car','abs@gmail.com','12/12/2020','11:10:02','12:23:23'),
+  createData(8,1,'Buying new car','abs@gmail.com','12/12/2020','11:10:02','12:23:23'),
+  createData(8,1,'Buying new car','abs@gmail.com','12/12/2020','11:10:02','12:23:23'),
+];
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 700,
+    minHeight:300
+  },
+});
+
+export default function CustomizedTables() {
+  const classes = useStyles();
+
+  return (
+    <TableContainer component={Paper}>
+      <Table className={classes.table} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell align ='center'>Serial_No</StyledTableCell>
+            <StyledTableCell align="center">Meeting_Id</StyledTableCell>
+            <StyledTableCell align="center">Subject_of_Mail</StyledTableCell>
+            <StyledTableCell align="center">Customer_Emailid</StyledTableCell>
+            <StyledTableCell align="center">Dealer_enduser_slot_start_date</StyledTableCell>
+            <StyledTableCell align="center">Dealer_enduser_slot_start_time</StyledTableCell>
+            <StyledTableCell align="center">Dealer_enduser_slot_end_date</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <StyledTableRow key={row.Serial_No}>
+              <StyledTableCell align="center" component="th" scope="row">
+                {row.Serial_No}
+              </StyledTableCell>
+              <StyledTableCell align="center">{row.Meeting_Id}</StyledTableCell>
+              <StyledTableCell align="center">{row.Subject_of_Mail}</StyledTableCell>
+              <StyledTableCell align="center">{row.Customer_Emailid}</StyledTableCell>
+              <StyledTableCell align="center">{row.Dealer_enduser_slot_start_date}</StyledTableCell>
+              <StyledTableCell align="center">{row.Dealer_enduser_slot_start_time}</StyledTableCell>
+              <StyledTableCell align="center">{row.Dealer_enduser_slot_end_time}</StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
